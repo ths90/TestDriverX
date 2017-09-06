731855df0c78e3c04fc1cc994602328db295c9c5
Thiago Santos <th.santos90@hotmail.com>
2017-09-05 21:50:35 -0300

    Atualização TestDrive

---
+++ TestDrive/TestDrive/TestDrive/Views/DetalheView.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+
+namespace TestDrive.Views
+{
+	
+	public partial class DetalheView : ContentPage
+	{
+		public Veiculo Veiculo { get; set; }
+
+        private const int VALOR_FREIO_ABS = 800;
+        private const int VALOR_AR_CONDICIONADO = 1000;
+        private const int VALOR_MP3_PLAYER = 500;
+
+        public string TextoFreioABS
+        {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0}", VALOR_FREIO_ABS);
+            }
+        }
+
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R$ {0}", VALOR_AR_CONDICIONADO);
+            }
+        }
+
+        public string TextoMP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", VALOR_MP3_PLAYER);
+            }
+        }
+        bool temFreioABS;
+
+        public bool TemFreioABS
+        {
+            get
+            {
+                return temFreioABS;
+            }
+            set
+            {
+                temFreioABS = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PrecoTotalFormatado));
+            }        
+        }
+
+        bool temArCondicionado;
+
+        public bool TemArCondicionado
+        {
+
+        }
+
+    }
+}
